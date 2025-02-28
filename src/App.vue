65367c34b32aa6a9766a2d7d9a518fbcea51f42f
kwkscream <spoonkicker@gmail.com>
2025-02-28 16:02:09 +0200

    fixed vercel. Added Vercel Analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Header from "./components/Header.vue";
+import { Analytics } from '@vercel/analytics/vue';
 </script>
 
 <template>
+    <Analytics />
     <Header />
     <main class="p-4 h-[700px]">
       <RouterView />
