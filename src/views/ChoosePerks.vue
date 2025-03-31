46c620c1e6d2c37251b46ead13d3895502f84661
kwkscream <spoonkicker@gmail.com>
2025-03-31 18:23:04 +0300

    changed data files

--- src/views/ChoosePerks.vue
+++ src/views/ChoosePerks.vue
@@ -1,30 +1,28 @@
 <script setup>
 import DragDrop from "../components/DragDrop.vue";
 
-
 import { store } from "../store";
 import { ref, onMounted } from "vue";
-import { dronemaster } from "@/data/skills/Dronemaster.js";
-import { exterminator } from "@/data/skills/Exterminator.js";
-import { fixer } from "@/data/skills/Fixer.js";
-import { medic } from "@/data/skills/Medic.js";
-import { slasher } from "@/data/skills/Slasher.js";
-import { hellraiser } from "@/data/skills/Hellraiser.js";
-import { vanguard } from "@/data/skills/Vanguard.js";
-import { gunslinger } from "@/data/skills/Gunslinger"
 
+import dronemaster from "@/data/skills/Dronemaster.json";
+import exterminator from "@/data/skills/Exterminator.json";
+import fixer from "@/data/skills/Fixer.json";
+import medic from "@/data/skills/Medic.json";
+import slasher from "@/data/skills/Slasher.json";
+import hellraiser from "@/data/skills/Hellraiser.json";
+import vanguard from "@/data/skills/Vanguard.json";
+import gunslinger from "@/data/skills/Gunslinger.json";
 
-
-const Classes = {	
-	"Dronemaster": dronemaster,
-	"Gunslinger" : gunslinger,
-	"Exterminator": exterminator,
-	"Fixer": fixer,
-	"Medic": medic,
-	"Slasher": slasher,
-	"Hellraiser": hellraiser,
-	"Vanguard" : vanguard,
-}
+const Classes = {
+	Dronemaster: dronemaster,
+	Gunslinger: gunslinger,
+	Exterminator: exterminator,
+	Fixer: fixer,
+	Medic: medic,
+	Slasher: slasher,
+	Hellraiser: hellraiser,
+	Vanguard: vanguard,
+};
 
 const isPrestige = ref(false);
 const hoveredSkill = ref(null);
@@ -36,7 +34,6 @@ const selectedSkills = ref({
 
 const doubleSelect = (skill, i) => {
 	if (skill.isPassive) {
-
 		const exists = selectedSkills.value.passive.some(
 			(f) => f.name === skill.name,
 		);
@@ -46,10 +43,12 @@ const doubleSelect = (skill, i) => {
 				(f) => f.name !== skill.name,
 			);
 		} else {
-			selectedSkills.value.passive = [...selectedSkills.value.passive, skill];
+			selectedSkills.value.passive = [
+				...selectedSkills.value.passive,
+				skill,
+			];
 		}
 	} else {
-
 		const updatedActive = { ...selectedSkills.value.active };
 
 		if (updatedActive[i]?.name === skill.name) {
@@ -65,14 +64,14 @@ const doubleSelect = (skill, i) => {
 };
 
 const exportToJson = () => {
-	const dataToExport = { 
-		...selectedSkills.value, 
-		className: store.selectedClass.name 
+	const dataToExport = {
+		...selectedSkills.value,
+		className: store.selectedClass.name,
 	};
 
-	const dataStr = JSON.stringify(dataToExport, null, 2); 
+	const dataStr = JSON.stringify(dataToExport, null, 2);
 	const blob = new Blob([dataStr], { type: "application/json" });
-	const url = URL.createObjectURL(blob); 
+	const url = URL.createObjectURL(blob);
 
 	const a = document.createElement("a");
 	a.href = url;
@@ -81,7 +80,6 @@ const exportToJson = () => {
 	a.click();
 	document.body.removeChild(a);
 };
-
 
 const importFromJson = (event) => {
 	isPrestige.value = event.passive.some((skill) => skill.isPrestige);
@@ -111,78 +109,96 @@ onMounted(() => {
 	<DragDrop />
 	<!-- інфа шо за клас пікнули -->
 	<div class="flex-col items-center justify-center h-screen">
-	<div class="flex justify-center items-center gap-2">
-		<h2 class="text-4xl font-bold mb-4 text-white text-center">
-			{{$t('selected_class')}} -
-			<span class="text-red-600">{{ store.selectedClass.name }}</span>
-		</h2>
-		<img width="63" height="57" :src="store.selectedClass.image" />
-	</div>
-	<div class="flex items-center justify-center mt-2 mb-5 flex-col">
-		<button
-			@click="exportToJson"
-			class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-		>
-			{{$t('save_build')}}
-		</button>
-		<div class="flex items-center justify-center">
-			<input
-				v-model="isPrestige"
-				id="default-checkbox"
-				type="checkbox"
-				value=""
-				class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-			/>
-			<label
-				for="default-checkbox"
-				class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-				>{{$t('prestige')}}</label
-			>
+		<div class="flex justify-center items-center gap-2">
+			<h2 class="text-4xl font-bold mb-4 text-white text-center">
+				{{ $t("selected_class") }} -
+				<span class="text-red-600">{{ store.selectedClass.name }}</span>
+			</h2>
+			<img width="63" height="57" :src="store.selectedClass.image" />
 		</div>
-	</div>
-	<div class="flex justify-center">
-		<div class="flex gap-2">
-			<div v-for="(col, i) in Classes[store.selectedClass.name]" class="flex gap-2 flex-col">
+		<div class="flex items-center justify-center mt-2 mb-5 flex-col">
+			<button
+				@click="exportToJson"
+				class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+			>
+				{{ $t("save_build") }}
+			</button>
+			<div class="flex items-center justify-center">
+				<input
+					v-model="isPrestige"
+					id="default-checkbox"
+					type="checkbox"
+					value=""
+					class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+				/>
 				<label
-					v-for="(skill, skillIndex) in col"
-					:key="skill.name + renderKey"
-					:class="[
-						'w-22 h-22 rounded-full border-3 transition flex items-center justify-center',
-						'hover:border-white',
-						'bg-gray-500',
-						'border-gray-500',
-						'has-checked:border-green-500',
-						'relative cursor-pointer',
-						skill.isPrestige && !isPrestige ? '!border-gray-600 brightness-30' : '',
-						skill.isPassive ? 'border-red-600' : '',
-					]"
-					@mouseenter="hoveredSkill = skill"
-					@mouseleave="hoveredSkill = null"
+					for="default-checkbox"
+					class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+					>{{ $t("prestige") }}</label
 				>
-				<img :src="'/nodes-icons/' + store.selectedClass.name + '/' + skill.image + '.webp'" alt="Skill Image" class="rounded-[1000px]">
-					<input
-						type="radio"
-						:name="skill.isPassive ? skill.name : i"
-						:disabled="skill.isPrestige && !isPrestige"
-						@click="doubleSelect(skill, i)"
-						:checked="
-							skill.isPassive
-								? selectedSkills.passive.some((s) => s.name === skill.name)
-								: selectedSkills.active[i]?.name === skill.name
-						"
-						class="sr-only"
-					/>
-				</label>
 			</div>
 		</div>
-	</div>
-	<div
-		v-if="hoveredSkill"
-		class="text-center mx-auto w-[600px] mt-[20px] p-2 border border-gray-200 bg-gray-800 text-white rounded"
-	>
-		<h3 class="font-bold">{{ hoveredSkill.name }}</h3>
-		<p>{{ hoveredSkill.description }}</p>
-	</div>
+		<div class="flex justify-center">
+			<div class="flex gap-2">
+				<div
+					v-for="(col, i) in Classes[store.selectedClass.name]"
+					class="flex gap-2 flex-col"
+				>
+					<label
+						v-for="(skill, skillIndex) in col"
+						:key="skill.name + renderKey"
+						:class="[
+							'w-22 h-22 rounded-full border-3 transition flex items-center justify-center',
+							'hover:border-white',
+							'bg-gray-500',
+							'border-gray-500',
+							'has-checked:border-green-500',
+							'relative cursor-pointer',
+							skill.isPrestige && !isPrestige
+								? '!border-gray-600 brightness-30'
+								: '',
+							skill.isPassive ? 'border-red-600' : '',
+						]"
+						@mouseenter="hoveredSkill = skill"
+						@mouseleave="hoveredSkill = null"
+					>
+						<img
+							:src="
+								'/nodes-icons/' +
+								store.selectedClass.name +
+								'/' +
+								skill.image +
+								'.webp'
+							"
+							alt="Skill Image"
+							class="rounded-[1000px]"
+						/>
+						<input
+							type="radio"
+							:name="skill.isPassive ? skill.name : i"
+							:disabled="skill.isPrestige && !isPrestige"
+							@click="doubleSelect(skill, i)"
+							:checked="
+								skill.isPassive
+									? selectedSkills.passive.some(
+											(s) => s.name === skill.name,
+										)
+									: selectedSkills.active[i]?.name ===
+										skill.name
+							"
+							class="sr-only"
+						/>
+					</label>
+				</div>
+			</div>
+		</div>
+		<div
+			v-if="hoveredSkill"
+			class="text-center mx-auto w-[600px] mt-[20px] p-2 border border-gray-200 bg-gray-800 text-white rounded"
+		>
+			<h3 class="font-bold">{{ hoveredSkill.name }}</h3>
+			<p>{{ hoveredSkill.description }}</p>
+		</div>
 	</div>
 </template>
 
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,24 +1,21 @@
 <script setup>
 import { store, classes } from "../store";
-
-
-
 </script>
 
 <template>
 	<div class="flex-col items-center justify-center">
 		<div class="max-w-[1500px] w-full mx-auto">
 			<h1 class="text-4xl font-bold mb-4 text-white text-center">
-				{{$t('title')}}
-				<span class="text-red-600">{{$t('build')}}</span>
+				{{ $t("title") }}
+				<span class="text-red-600">{{ $t("build") }}</span>
 			</h1>
 			<p class="text-gray-400 w-xl mx-auto text-center mb-[35px]">
-				{{$t('sub_title')}}
+				{{ $t("sub_title") }}
 			</p>
 
 			<div class="max-w-md mx-auto mt-10 p-6">
 				<h2 class="text-4xl font-bold mb-10 text-white text-center">
-					{{$t('choose_class')}}
+					{{ $t("choose_class") }}
 				</h2>
 				<div class="flex gap-3">
 					<img
@@ -45,7 +42,7 @@ import { store, classes } from "../store";
 
 				<div class="mt-6" v-if="store.selectedClass">
 					<h3 class="text-2xl font-bold text-red-500 mb-2">
-						{{$t('weapons')}}
+						{{ $t("weapons") }}
 					</h3>
 					<div
 						class="bg-red-900 p-3 rounded-lg flex items-center justify-center"
@@ -59,7 +56,7 @@ import { store, classes } from "../store";
 
 					<div class="mt-6" v-if="store.selectedClass">
 						<h3 class="text-2xl font-bold text-red-500 mb-2">
-							{{$t('equipment')}}
+							{{ $t("equipment") }}
 						</h3>
 						<div
 							class="bg-gray-800 p-3 rounded-lg flex items-center justify-center"
@@ -72,7 +69,7 @@ import { store, classes } from "../store";
 							:to="{ name: 'choosePerks' }"
 							class="mt-6 inline-block bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-lg transition duration-300 w-full text-center"
 						>
-							{{$t('to_build')}}
+							{{ $t("to_build") }}
 						</router-link>
 					</div>
 				</div>
